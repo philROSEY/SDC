26fa3e3b6077fe7515a8170add27a2b19607f478
Philip <philip.rosenberg@comcast.netgit config --global core.editor vim>
2020-12-10 19:45:30 -0500

    changed connectionLimit to 8

--- philServer/db/index.js
+++ philServer/db/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const conn = mysql.createPool({
-    connectionLimit: 15,
+    connectionLimit: 8,
     host: '3.17.141.94',
     user: 'root',
     password: 'password',
